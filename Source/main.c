56c53b276a016fb13cd762a36c2f472cecdc552d
Frank Tkalcevic <frank@franksworkshop.com.au>
2018-07-05 16:09:27 +1000

    Test buzzers.  And the winner is... MCSMT-8540C-3716

--- Source/main.c
+++ Source/main.c
@@ -23,6 +23,18 @@
 
 #define SETUP_SAMPLES 16
 
+// Use PNP for output (active low)
+#define PWM_INVERTED
+
+// Select frequency (or none for dc buzzer)
+//#define PWM300
+#define PWM2670
+//#define PWM2730
+//#define PWM2830
+//#define PWM4000
+//#define PWM4400
+
+#define PWM (defined(PWM2730) || defined(PWM4000) || defined(PWM4400) || defined(PWM300) || defined(PWM2670) || defined(PWM2830))
 
 struct EEPROMData
 {
@@ -30,13 +42,68 @@ struct EEPROMData
 	uint16_t low_voltage;
 };
 
-uint8_t calcCRC(uint16_t value)
+static uint8_t calcCRC(uint16_t value)
 {
 	uint8_t crc = 0x72;
 	crc = _crc_ibutton_update(crc, value & 0xFF);
 	crc = _crc_ibutton_update(crc, value >> 8);
 	return crc;
 }
+
+static void PWMOn()
+{
+#if PWM
+	// Setup tranducer timer1 -4.4kHz
+	TCCR1 = _BV(CTC1) |					// Clear counter on OCR1C match
+#if defined(PWM300)
+			_BV(CS11);					// Prescale /2
+#else
+			_BV(CS10);					// Prescale /1
+#endif
+	GTCCR = _BV(PWM1B) |				// PWM mode
+			_BV(COM1B0);				// toggle oc1b
+
+	#ifdef PWM4400
+		OCR1B = 14;	//4400
+		OCR1C = 28;
+	#elif defined(PWM2670)
+		OCR1B = 23;	//2670
+		OCR1C = 47;
+	#elif defined(PWM2730)
+		OCR1B = 23;	//2730
+		OCR1C = 46;
+	#elif defined(PWM2830)
+		OCR1B = 22;	//2730
+		OCR1C = 44;
+	#elif defined(PWM4000)
+		OCR1B = 15;	//4000
+		OCR1C = 31;
+	#elif defined(PWM300)
+		OCR1B = 104;	//300
+		OCR1C = 207;
+	#endif
+#else
+	#ifdef PWM_INVERTED
+		PORTB &= ~_BV(BUZZER_PIN);
+	#else
+		PORTB |= _BV(BUZZER_PIN);
+	#endif
+#endif
+}
+
+static void PWMOff()
+{
+#if PWM
+	GTCCR = 0;
+	TCCR1 = 0;				// clk stopped
+#endif
+	#ifdef PWM_INVERTED
+		PORTB |= _BV(BUZZER_PIN);
+	#else
+		PORTB &= ~_BV(BUZZER_PIN);
+	#endif
+}
+
 
 ISR(WDT_vect)
 {
@@ -65,14 +132,6 @@ int main(void)
 	DIDR0 = _BV(ADC3D);
 	_delay_ms(1);
 
-	// Setup tranducer timer1 -4.4kHz
-	TCCR1 = _BV(CTC1) |					// Clear counter on OCR1C match
-			0;							// clock stopped
-	GTCCR = _BV(PWM1B) |				// PWM mode
-			_BV(COM1B0);				// toggle oc1b
-	OCR1B = 14;
-	OCR1C = 28;
-
 
 	// throw the first value away
 	ADCSRA |= _BV(ADSC);
@@ -82,6 +141,10 @@ int main(void)
 	PRR |= _BV(PRUSI);	// we don't use USI
 	PRR |= _BV(PRTIM0);	// we don't use TIMER0
 
+#if 0
+	PWMOn();
+	for(;;){}
+#endif
 
 	struct EEPROMData data;
 	eeprom_read_block( &data, 0, sizeof(data) );
@@ -134,16 +197,15 @@ int main(void)
 			PORTB |= _BV(LED_PIN);
 			//PORTB |= _BV(BUZZER_PIN);
 			if ( beeper & 1 )
-				TCCR1 &= ~_BV(CS10);				// clk stopped
+				PWMOff();
 			else
-				TCCR1 |= _BV(CS10);					// Prescale /1
+				PWMOn();
 			beeper++;
 		}
 		else
 		{
 			PORTB &= ~_BV(LED_PIN);
-			//PORTB &= ~_BV(BUZZER_PIN);
-			TCCR1 &= ~_BV(CS10);				// clk stopped
+			PWMOff();
 			beeper = 0;
 		}
 		
